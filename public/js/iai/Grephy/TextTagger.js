8eb140ac6aa89d01f498645e30b95bce0d1277ae
Ron Itelman <ron@jamr.com>
2018-09-27 14:51:08 -0600

    switching to versions

--- public/js/iai/Grephy/TextTagger.js
+++ public/js/iai/Grephy/TextTagger.js
@@ -4,3 +4,14 @@ window.TextTagger = TextTagger;
 TextTagger.test = function() {
     console.log("TextTagger active");
 };
+
+TextTagger.init = function() {
+    let selector = `[data-texttagger-power="on"]`;
+    let elems = document.querySelectorAll(selector);
+    let (elem of elems) {
+        console.log(elem);
+    }
+    console.log("I am going to scan the DOM for any elements with the attribute data-tt and output their value");
+}
+
+TextTagger.init();
