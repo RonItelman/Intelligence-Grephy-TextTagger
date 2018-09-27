72830e63d150cfe11c172674fd1243fc9dced526
Ron Itelman <ron@jamr.com>
2018-09-27 16:26:31 -0600

    can highlight text

---
+++ public/data/apiReturn.js
@@ -0,0 +1,15 @@
+let data = {
+    "input":"Predict inventory sales for the 2018 holiday season.",
+    "phrases":[
+        {
+            "text":"Predict inventory sales",
+            "type":"QUANTITY",
+            "description":"Measurements, as of weight or distance."
+        },
+        {
+            "text":"2018 holiday season",
+            "type":"EVENT",
+            "description":"Named hurricanes, battles, wars, sports events, etc."
+        }
+    ]
+}
--- public/js/iai/Grephy/TextTagger.js
+++ public/js/iai/Grephy/TextTagger.js
@@ -5,13 +5,61 @@ TextTagger.test = function() {
     console.log("TextTagger active");
 };
 
+TextTagger.highlighterColors = [
+    "#fdff88",
+    "#a5ff8a",
+    "#8cf0ff",
+    "#8f9eff",
+    "#ff91e9",
+    "#ff9f94"
+]
+
+TextTagger.query = function({phrase, input}) {
+    // let i = input.search(phrase);
+    // console.log(phrase, input, i);
+    phrase.map(function(index) {
+        console.log(index, phrase[index]);
+    })
+    // let matches = input.split(phrase);
+    // for(let match of matches) {
+    //     console.log(match);
+    // }
+};
+
+TextTagger.highlightRange = function(el, start, end) {
+    var text = el.textContent.trim()
+    el.innerHTML = text.substring(0, start) + 
+      '<span style="background:yellow">' +
+      text.substring(start, end) + 
+      "</span>" + text.substring(end);
+}
+
+TextTagger.selectTextByRange = function (obj, start, stop) {
+    var endNode, startNode = endNode = obj.firstChild
+  
+    startNode.nodeValue = startNode.nodeValue.trim();
+    
+    var range = document.createRange();
+    range.setStart(startNode, start);
+    range.setEnd(endNode, stop + 1);
+    
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+ };
+
 TextTagger.init = function() {
     let selector = `[data-texttagger-power="on"]`;
-    let elems = document.querySelectorAll(selector);
-    let (elem of elems) {
-        console.log(elem);
-    }
-    console.log("I am going to scan the DOM for any elements with the attribute data-tt and output their value");
+    let elem = document.querySelector('[data-texttagger-power="on"]');
+    // TextTagger.selectTextByRange(elem, 3, 10);
+    TextTagger.highlightRange(elem, 3, 10)
+    // let phrases = data.phrases;
+    // for (let phrase of phrases) {
+    //     let text = phrase.text;
+    //     TextTagger.query({phrase:text, input:data.input});
+    //     let type = phrase.type;
+    //     let desc = phrase.description;        
+    // }
 }
 
 TextTagger.init();
