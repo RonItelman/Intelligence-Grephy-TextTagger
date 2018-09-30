d06a8c6cdb7160dbe135848e3cef207d1b6a306f
Ron Itelman <ron@jamr.com>
2018-09-30 16:13:56 -0600

    working on electron

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
+  mainWindow = new BrowserWindow({width: 411, height})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
