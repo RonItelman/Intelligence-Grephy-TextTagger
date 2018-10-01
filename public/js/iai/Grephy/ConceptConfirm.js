bf14d2be16d07ec2f78cc74165accf94f30c135b
Ron Itelman <ron@jamr.com>
2018-10-01 12:02:59 -0600

    switching context

---
+++ public/js/iai/Grephy/ConceptConfirm.js
@@ -0,0 +1,6 @@
+let ConceptConfirm = {}; //contains all TextTagger functions.
+window.ConceptConfirm = ConceptConfirm; //assign to global window
+
+ConceptConfirm.test = function() {
+    console.log("ConceptConfirm reply");
+}
